f0f44adf66cfc4ca0bf16c17c4ab5ad6e15dcf26
thiago-couto <thiagoferreira2508@gmail.com>
2025-04-03 16:26:18 -0300

    Update dados.py

--- dados.py
+++ dados.py
@@ -1,7 +1,7 @@
 import pandas as pd 
  
  # Carregar os dados de um arquivo CSV
- file_path = "/mnt/data/The Impacts of Seleção de Dados (Base INEP).csv" 
+ file_path = "/mnt/data/Seleção de Dados (Base INEP).csv" 
  df = pd.read_csv(file_path) 
  # Selecionar dados com base em uma condição
  dados_selecionados = df[df['coluna A'] == 'valor_desejado']
